ca35158a0d725af8d250cf887e9356bb41e092ca
prakashceg <prakash.mcn@gmail.com>
2023-07-03 16:15:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,4 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing:
-
 streamlit.dataframe(my_fruit_list)
